34f38362bf96379ed36c22b770ac5cea4a7362d5
NYC <yibarra0090>
2018-11-20 20:13:56 -0500

    Done!?

---
+++ src/Runner.java
@@ -0,0 +1,16 @@
+public class Runner {
+    public static void main(String[] args){
+     StringChecker sc1 = new CodeWordChecker(5,6,"$");
+     StringChecker sc2 = new CodeWordChecker("pass");
+
+     System.out.println(sc1.isValid("happy"));
+     System.out.println(sc1.isValid("happy$"));
+     System.out.println(sc1.isValid("Code"));
+     System.out.println(sc1.isValid("happyCode"));
+
+     System.out.println(sc2.isValid("MyPass"));
+     System.out.println(sc2.isValid("MypassPort"));
+     System.out.println(sc2.isValid("happy"));
+     System.out.println(sc2.isValid("1,000,000,000,000,000"));
+    }
+}
